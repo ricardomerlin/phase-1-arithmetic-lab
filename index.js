9e3509d7183101ead23355fbbac4fafafe0e2633
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-10-30 19:55:31 -0400

    Uploading arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.ceil(Math.random() * 10);
+
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3%num4;
+
+let max = Math.max(1, 4, 5, 6, 20);
